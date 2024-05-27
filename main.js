a416ce85065f71a7a04583c82d08b16a1bc8da0c
Red_Robles <redworld22@hotmail.com>
2024-05-27 11:08:29 -0400

    fusion aside con documento principal, correcto de una etiqueta de cierre de más en el documento clase13.html

--- main.js
+++ main.js
@@ -2,15 +2,40 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideMenu = document.querySelector('.product-detail');
+const cartbtn = document.querySelector('.navbar-shopping-cart')
 
 menuEmail.addEventListener('click', toggleDesktopmenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+cartbtn.addEventListener('click', toggleAsideMenu);
+
 
 function toggleDesktopmenu() {
+    const isAsideMenuClosed = asideMenu.classList.contains('inactive');
+
+    if(!isAsideMenuClosed){
+        asideMenu.classList.add('inactive');
+    };
+
     desktopMenu.classList.toggle('inactive');
 }
 
-menuHamIcon.addEventListener('click', togglemobileMenu);
+function toggleMobileMenu() {
+    const isAsideMenuClosed = asideMenu.classList.contains('inactive');
 
-function togglemobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+    if(!isAsideMenuClosed){
+        asideMenu.classList.add('inactive');
+    };
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideMenu() {
+    isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    asideMenu.classList.toggle('inactive');
 }
